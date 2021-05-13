df102da7a9c1c73a7a95306e6f6ae2079771f991
prerana1821 <prerananawar1@gmail.com>
2021-05-13 17:33:12 +0530

    style: added typescript

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Varela Round"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
